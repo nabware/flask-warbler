45f29e29523ab6dfc9babcdb6da8b14191814870
Axa Pickering <axapickering@gmail.com>
2023-10-12 12:14:28 -0700

    added test for follow user

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,7 @@ import os
 from unittest import TestCase
 
 from models import db, User, Message, Follow
+from sqlalchemy import and_
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -19,7 +20,9 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 # Now we can import app
 
-from app import app
+from app import app, CURR_USER_KEY
+
+app.config['WTF_CSRF_ENABLED'] = False
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -37,6 +40,8 @@ class UserModelTestCase(TestCase):
         u2 = User.signup("u2", "u2@email.com", "password", None)
 
         db.session.commit()
+        self.u1 = u1
+        self.u2 = u2
         self.u1_id = u1.id
         self.u2_id = u2.id
 
@@ -49,3 +54,24 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_start_following(self):
+        ''' Tests attribute updating for user following another user'''
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+
+            response = c.post(f'/users/follow/{self.u2_id}', headers={
+                    "referer" : "/"
+            },follow_redirects=True)
+
+            self.assertEqual(response.status_code,200)
+            html = response.get_data(as_text=True)
+
+            self.assertIn('<!-- HOMEPAGE :: FOR TESTING :: DO NOT MOVE -->',html)
+
+            Follow.query.filter(
+                and_(Follow.user_being_followed_id==self.u2_id,
+                        Follow.user_following_id==self.u1_id)).one()
